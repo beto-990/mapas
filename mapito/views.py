8caecc886e35b049f221bc00050033730fc29b59
betop <betopbr@outlook.com>
2021-01-14 17:34:59 -0300

    a

--- mapito/views.py
+++ mapito/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import Estado
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'mapito/post_list.html', {})
+    estados = Estado.objects.filter(sigla='SC').order_by('pop')
+    return render(request, 'mapito/post_list.html', {'estados': estados})
